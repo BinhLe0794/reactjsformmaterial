82ebd7c874102064c5d6cbd264f40f8e6585b9ba
Binh Le <huybinh0794@gmail.com>
2022-05-02 16:18:44 +0700

    dispatch action into view

--- src/features/Product/pages/DetailPage.jsx
+++ src/features/Product/pages/DetailPage.jsx
@@ -1,19 +1,31 @@
 import { Box, Container, Grid, LinearProgress, Paper } from "@mui/material";
 import React from "react";
+import { useDispatch } from "react-redux";
 import { Outlet, useParams } from "react-router-dom";
 import AddToCardForm from "../components/AddToCardForm";
 import ProductInfo from "../components/ProductInfo";
 import ProductMenu from "../components/ProductMenu";
 import ProductThumbnail from "../components/ProductThumbnail";
 import useProductDetail from "../hooks/userProductDetail";
+import { addToCart } from "../../Cart/cartSlice";
 
 const DetailPage = (props) => {
   const params = useParams();
   const productId = params.id;
-
+  const dispatch = useDispatch();
+  //Call API
   const { product, loading } = useProductDetail(productId);
-  const handleAddToCardSubmit = (formValue) => {
-    console.log("formValue", formValue);
+
+  // destructuring formValues
+  const handleAddToCardSubmit = ({ quantity }) => {
+    // console.log("formValue", formValue);
+    const action = addToCart({
+      id: product.id,
+      product,
+      quantity,
+    });
+    console.log(action);
+    dispatch(action);
   };
   return (
     <Box>
